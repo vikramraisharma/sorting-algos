61c6b76b7382ffbe94deb67a706480c43b0929a1
Vikram Sharma <vikram.sharma1024@gmail.com>
2020-11-12 14:26:12 -0500

    insertion sort added

--- insertion-sort.js
+++ insertion-sort.js
@@ -9,26 +9,28 @@ const nums = [1,9,2,8,3,7,4,6]
     and ensuring it is in the right numerical order.
 */
 
-function insertionSort(array){
-    // iterate through nums
+function insertionSort(array) {
+    // Write your code here.
+      // iterate through the array
       for(let i = 1; i < array.length; i++){
-          // set j to previous index, used to compare values
-          let j = i - 1;
-          // iterate through the subarray for each value
-          // determine if the previous values are > the current
-          while(j >= 0 && array[j] > array[i]){
-              // use j to order the values in the sorted subarray
-              array[j + 1] = array[j];
-              j = j -1;
+          // set j to the current starting index (i)
+          let j = i;
+          // use swap func to ensure that array[j] is the lower value
+          while (j > 0 && array[j] < array[j-1]){
+              swap(j, j-1, array)
+              // this normalizes the difference between i and j
+              j -= 1;
           }
-          // put the value in its new place in the sorted subarray
-          array[j + 1] = array[i]
       }
       return array
   }
-  /*
-    No swap function is needed here, because we can work with j
-    which is an index placeholder that changes based on its order
-    in the sorted subarray.
-  */
-  console.log(insertionSort(nums));
+  
+  // swapping the values directly in the array instead of just swapping the indeces
+  function swap(i, j, array){
+      const temp = array[j]
+      array[j] = array[i]
+      array[i] = temp
+  }
+  
+ 
+ os.remove(text.name)
